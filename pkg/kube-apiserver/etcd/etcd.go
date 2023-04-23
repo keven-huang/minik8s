8b2157dd65e3c534c68dd22a54416871b0f7336b
keven-huang <18201853040@outlook.com>
2023-04-23 20:01:53 +0800

    feat(apiserver): pod create/add handler

--- pkg/kube-apiserver/etcd/etcd.go
+++ pkg/kube-apiserver/etcd/etcd.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"time"
 
-	"go.etcd.io/etcd/client/v3"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 var (
@@ -18,15 +18,10 @@ type EtcdStore struct {
 	client *clientv3.Client
 }
 
-type Result struct {
-	key string
-	val string
-}
-
 // constructort of etcdstore
 func InitEtcdStore() (*EtcdStore, error) {
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"http://127.0.0.1:2379"},
+		Endpoints:   []string{"http://127.0.0.1:30000"},
 		DialTimeout: 5 * time.Second,
 	})
 	fmt.Printf("in\n")
@@ -55,7 +50,7 @@ func (store *EtcdStore) Put(key string, val string) error {
 }
 
 // operations : get
-func (store *EtcdStore) Get(key string) ([]Result, error) {
+func (store *EtcdStore) Get(key string) ([]string, error) {
 	fmt.Printf("in\n")
 	kv := clientv3.NewKV(store.client)
 	resp, err := kv.Get(context.TODO(), key)
@@ -63,22 +58,15 @@ func (store *EtcdStore) Get(key string) ([]Result, error) {
 		fmt.Printf("get to etcd failed, err:%v\n", err)
 		log.Fatal(err)
 	}
-	for _, v := range resp.Kvs {
-		fmt.Printf("type: %s key: %s value: %s\n", "GET", v.Key, v.Value)
-	}
 	if len(resp.Kvs) == 0 {
-		return []Result{}, err
+		return []string{}, err
 	} else {
-		res := []Result{}
+		res := []string{}
 		for _, ev := range resp.Kvs {
-			res = append(res, Result{
-				key: string(ev.Key),
-				val: string(ev.Value),
-			})
+			res = append(res, string(ev.Value))
 		}
 		return res, err
 	}
-	return []Result{}, err
 }
 
 // operation : del
@@ -99,3 +87,5 @@ func (store *EtcdStore) Listen(key string) {
 		}
 	}
 }
+
+// perfix watch
