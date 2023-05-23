35ede530a92393c2d7276a79d6063434a2c3d84e
JinboHan <chasingdreams@sjtu.edu.cn>
2023-05-23 09:20:58 +0800

    fix: add make scripts and ci/cd

---
+++ Makefile
@@ -0,0 +1,13 @@
+clean:
+	etcdctl del "/api" --prefix
+
+run:
+	go run ./cmd/kube-apiserver/kube-apiserver.go > log/apiserver.log &
+	sleep 2
+	go run ./cmd/kube-scheduler/kube-scheduler.go > log/scheduler.log &
+	go run ./cmd/kubelet/kublet.go > log/kubelet.log &
+	go run ./cmd/kube-service/kubeservice.go > log/kubeservice.log &
+	go run ./cmd/kube-proxy/kubeproxy.go > log/kubeproxy.log &
+
+stop:
+	./scripts/linux/stop.sh
--- cmd/kubelet/dockerClient/pullOneImage.go
+++ cmd/kubelet/dockerClient/pullOneImage.go
@@ -1,17 +1,13 @@
 package main
 
-import (
-	"minik8s/pkg/kubelet/config"
-	"minik8s/pkg/kubelet/dockerClient"
-)
-
-func main() {
-
-	//image := "alpine"
-	image := config.PAUSE_IMAGE_NAME
-
-	err := dockerClient.PullOneImage(image)
-	if err != nil {
-		panic(err.Error())
-	}
-}
+//
+//func main() {
+//
+//	//image := "alpine"
+//	image := config.PAUSE_IMAGE_NAME
+//
+//	err := dockerClient.PullOneImage(image)
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//}
