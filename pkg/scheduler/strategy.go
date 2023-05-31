a3bf1aad466c9f4baeb560e81b1764ede5ca6936
luhaoqi <1171821456@qq.com>
2023-05-31 21:20:54 +0800

    feat(serverless): update log in scheduler

--- pkg/scheduler/strategy.go
+++ pkg/scheduler/strategy.go
@@ -1,6 +1,7 @@
 package scheduler
 
 import (
+	"fmt"
 	"math/rand"
 	"minik8s/pkg/api/core"
 	"time"
@@ -10,6 +11,7 @@ var global_cnt int = 0
 
 func roundrobin_strategy(nodes []core.Node) string {
 	global_cnt = (global_cnt + 1) % len(nodes)
+	fmt.Println("[scheduler] [roundrobin_strategy] nodes:", nodes, "global_cnt:", global_cnt)
 	return nodes[global_cnt].Name
 }
 
