849e3f42822b84555e59ee83101f6f643b71a13b
luhaoqi <1171821456@qq.com>
2023-05-31 21:00:10 +0800

    feat(serverless): delete function informer in kubelet.go

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -25,10 +25,10 @@ type Kubelet struct {
 	Monitor      *monitor.Monitor
 	PodInformer  informer.Informer
 	NodeInformer informer.Informer
-	FunctionInformer informer.Informer
-	node         core.Node
-	workers      map[string]time.Time
-	masterIp     string
+	//FunctionInformer informer.Informer
+	node     core.Node
+	workers  map[string]time.Time
+	masterIp string
 }
 
 func NewKubelet(name string, nodeIp string, masterIp string) (*Kubelet, error) {
@@ -51,10 +51,10 @@ func NewKubelet(name string, nodeIp string, masterIp string) (*Kubelet, error) {
 		return nil, err
 	}
 	return &Kubelet{
-		Monitor:          monitor.NewMonitor(9400, &node),
-		PodInformer:      informer.NewInformer(apiconfig.POD_PATH),
-		FunctionInformer: informer.NewInformer(apiconfig.FUNCTION_PATH),
-		node:             node,
+		Monitor:     monitor.NewMonitor(9400, &node),
+		PodInformer: informer.NewInformer(apiconfig.POD_PATH),
+		//FunctionInformer: informer.NewInformer(apiconfig.FUNCTION_PATH),
+		node:         node,
 		NodeInformer: informer.NewInformer(apiconfig.NODE_PATH),
 		workers:      make(map[string]time.Time),
 		masterIp:     tmp,
@@ -108,67 +108,67 @@ func (k *Kubelet) Register() {
 	k.PodInformer.AddEventHandler(tool.Added, k.CreatePod)
 	k.PodInformer.AddEventHandler(tool.Modified, k.UpdatePod)
 	k.PodInformer.AddEventHandler(tool.Deleted, k.DeletePod)
-	k.FunctionInformer.AddEventHandler(tool.Added, k.CreateFunction)
-	k.FunctionInformer.AddEventHandler(tool.Modified, k.UpdateFunction)
-	k.FunctionInformer.AddEventHandler(tool.Deleted, k.DeleteFunction)
+	//k.FunctionInformer.AddEventHandler(tool.Added, k.CreateFunction)
+	//k.FunctionInformer.AddEventHandler(tool.Modified, k.UpdateFunction)
+	//k.FunctionInformer.AddEventHandler(tool.Deleted, k.DeleteFunction)
 }
 
-func (k *Kubelet) CreateFunction(event tool.Event) {
-	prefix := "[kubelet] [CreateFunction] "
-	// handle event
-	fmt.Println(prefix, "event.Type: ", tool.GetTypeName(event))
-	fmt.Println(prefix, "event.Key: ", event.Key)
-	k.FunctionInformer.Set(event.Key, event.Val)
-
-}
-
-func (k *Kubelet) UpdateFunction(event tool.Event) {
-	prefix := "[kubelet] [UpdateFunction] "
-	// handle event
-	fmt.Println(prefix, "event.Type: ", tool.GetTypeName(event))
-	fmt.Println(prefix, "event.Key: ", event.Key)
-	k.FunctionInformer.Set(event.Key, event.Val)
-
-	function := &core.Function{}
-	err := json.Unmarshal([]byte(event.Val), function)
-	if err != nil {
-		fmt.Println(prefix, "json.Unmarshal err: ", err)
-		return
-	}
-
-	// 镜像会自动覆盖
-
-	err = dockerClient.ImageBuild(function.Spec.FileDirectory, "luhaoqi/my_module:"+function.Name)
-	if err != nil {
-		fmt.Println(prefix, "dockerClient.ImageBuild err: ", err)
-		return
-	}
-
-	// TODO: 删除所有之前Function的容器
-}
-
-func (k *Kubelet) DeleteFunction(event tool.Event) {
-	prefix := "[kubelet] [DeleteFunction] "
-	// handle event
-	fmt.Println(prefix, "event.Type: ", tool.GetTypeName(event))
-	fmt.Println(prefix, "event.Key: ", event.Key)
-	val := k.FunctionInformer.Get(event.Key)
-	k.FunctionInformer.Delete(event.Key)
-
-	function := &core.Function{}
-	err := json.Unmarshal([]byte(val), function)
-	if err != nil {
-		fmt.Println(prefix, "json.Unmarshal err: ", err)
-		return
-	}
-
-	// TODO: image remove 目前还是空的
-	err = dockerClient.ImageRemove("my_module:" + function.Name)
-	if err != nil {
-		fmt.Println(prefix, "dockerClient.ImageRemove err: ", err)
-		return
-	}
-}
+//func (k *Kubelet) CreateFunction(event tool.Event) {
+//	prefix := "[kubelet] [CreateFunction] "
+//	// handle event
+//	fmt.Println(prefix, "event.Type: ", tool.GetTypeName(event))
+//	fmt.Println(prefix, "event.Key: ", event.Key)
+//	k.FunctionInformer.Set(event.Key, event.Val)
+//
+//}
+//
+//func (k *Kubelet) UpdateFunction(event tool.Event) {
+//	prefix := "[kubelet] [UpdateFunction] "
+//	// handle event
+//	fmt.Println(prefix, "event.Type: ", tool.GetTypeName(event))
+//	fmt.Println(prefix, "event.Key: ", event.Key)
+//	k.FunctionInformer.Set(event.Key, event.Val)
+//
+//	function := &core.Function{}
+//	err := json.Unmarshal([]byte(event.Val), function)
+//	if err != nil {
+//		fmt.Println(prefix, "json.Unmarshal err: ", err)
+//		return
+//	}
+//
+//	// 镜像会自动覆盖
+//
+//	//err = dockerClient.ImageBuild(function.Spec.FileDirectory, "luhaoqi/my_module:"+function.Name)
+//	//if err != nil {
+//	//	fmt.Println(prefix, "dockerClient.ImageBuild err: ", err)
+//	//	return
+//	//}
+//
+//	// TODO: 删除所有之前Function的容器
+//}
+//
+//func (k *Kubelet) DeleteFunction(event tool.Event) {
+//	prefix := "[kubelet] [DeleteFunction] "
+//	// handle event
+//	fmt.Println(prefix, "event.Type: ", tool.GetTypeName(event))
+//	fmt.Println(prefix, "event.Key: ", event.Key)
+//	val := k.FunctionInformer.Get(event.Key)
+//	k.FunctionInformer.Delete(event.Key)
+//
+//	function := &core.Function{}
+//	err := json.Unmarshal([]byte(val), function)
+//	if err != nil {
+//		fmt.Println(prefix, "json.Unmarshal err: ", err)
+//		return
+//	}
+//
+//	// TODO: image remove 目前还是空的
+//	err = dockerClient.ImageRemove("my_module:" + function.Name)
+//	if err != nil {
+//		fmt.Println(prefix, "dockerClient.ImageRemove err: ", err)
+//		return
+//	}
+//}
 
 // this should be a thread
 func (k *Kubelet) HandleConnection(conn net.Conn) {
@@ -432,7 +432,7 @@ func (k *Kubelet) Listener(needLog bool) {
 func (k *Kubelet) Run() {
 	go k.Monitor.Run()
 	go k.PodInformer.Run()
-	go k.FunctionInformer.Run()
+	//go k.FunctionInformer.Run()
 	go k.Listener(true)
 	if k.node.Labels["kind"] == "Master" {
 		go k.HeartBeatServer()
